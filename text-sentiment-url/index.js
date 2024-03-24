d673c3ff7b6081a3811e5febd4147257a63081e8
GUNNAM SURYA TEJA <81230818+GUNNAMSURYATEJA@users.noreply.github.com>
2024-03-24 10:58:31 -0400

    Delete text-sentiment-url directory

--- text-sentiment-url/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-const Routes = require('./routes');
-Routes.initialize(app);
-
-const serverPort = process.env.PORT || 8000;
-app.listen(serverPort, () => console.info(`on ${serverPort}`));
